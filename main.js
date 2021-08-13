904dbc59fde05e15af8ecb91d3aa69a71bc4cb5f
EliasYoc <eliyoc_97@hotmail.com>
2021-08-12 21:20:07 -0500

    a

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 console.warn("hola");
 console.log("esto es ejemplo");
+
+const saludo = () => {
+  return "hola";
+};
+
+console.log(saludo());
+
+const saludo2 = () => {
+  return "hola2";
+};
+
+console.log(saludo2());
+console.log("ultimo");
